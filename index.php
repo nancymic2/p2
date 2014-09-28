3254ac525ce37c0ec57e0297e645a750544bf9ab
nancy michell <michell@fas.harvard.edu>
2014-09-28 13:57:56 -0400

    add files

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<body style="font-size: 28">
+
+<form action="r.php" method="post">
+    Number of Words: <input type="number" name="numwords" maxlength=1 size="1"><br>
+
+   <input type="hidden" name="yesnumb" value="0">
+
+    Include number? <input type="checkbox" name="yesnumb" value="1"> <br>
+
+    <input type="hidden" name="yessymb" value="0">
+
+    Include special character? <input type="checkbox" name="yessymb" value="1"> <br>
+
+    <input type="submit">
+</form>
+
+</body>
+</html> 
