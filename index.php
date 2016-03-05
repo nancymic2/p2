cf849481a81b9c03d5b9f54e908d459f6437944e
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-05 10:14:40 -0500

    p2

--- index.php
+++ index.php
@@ -1,52 +1,67 @@
-<!doctype html> 
- <html lang="en"> 
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tennis with Federer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='http://fonts.googleapis.com/css?family=Cabin+Condensed:500' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="http://necolas.github.io/normalize.css/">
+	<link rel="stylesheet" href="css/styles.css">
+	<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="js/images2.js"></script>
+</head>
+<body>
+ <div class="container"> 
+ 	<header id="header1"><span id="headline">Play Tennis Like Roger Federer <img src="images/hamb.png" id="hamburger" alt="hamburger"></span>
+ 		<span id="ball">&nbsp;(</span>
+ 	</header>
+ 	<div id="header">		
+ 	</div>
+ 	<div id="smallnav">
+ 		<ul>
+ 			<li><a class="actlink" href="index.html">Home</a></li>
+ 			<li><a href="backhand.html">Backhand</a></li>
+ 			<li><a href="forehand.html">Forehand</a></li>
+ 			<li><a href="serve.html">Serve</a></li>
+            <li><a href="credits.html">Credits</a></li>
+ 		</ul>
+ 	</div>
+ 	<div id="nav">
+ <?php include("nav.php"); ?>
 
- <head> 
- <meta charset="utf-8"> 
- <title>Nancy Michell</title> 
- <link rel="stylesheet" type="text/css" href="css/css.css"> 
- </head> 
- <body> 
+ 	</div>
+ 	<div id="middle">
+
+ 	    <div class="twothird">Are you a tennis player? Do you want to play like Roger Federer?<br>
+ 	    	Federer is a seasoned player with several Wimbledon, US Open, French Open, and Australian Open titles in his career. He is originally from Basel Switzerland.<br><br>
+ 	    	Federer has faced all the contemporary tennis greats, including Rafael Nadal, Novak Djokovic, Andy Murray.
+ 	    	<br>Some of his stats include:
 
 
-<div id="div2">
-and on I go to my thesis project yay
+ 	    	<ul>
+ 	    		<li>17 major grand slam titles</li>
+				<li>First player with 300 career wins</li>
+ 	    		<li>7 Wimbledon titles</li>
+ 	    		<li>win-loss record: 1067:240</li>
+ 	    		<li>Tournaments played: 344</li>
+ 	    	</ul>
 
-
+ 	    </div>
+ 	     	    <div id="backh" class="third"><img src="images/Wimbledon2008.jpg" data-large="images/Wimbledon2008.jpg" data-small="images/Wimbledon2008.jpg" data-med="images/fedAndNadalMed.jpg" alt="tennis" class='serveImg'>
+ 	    </div>
+    </div>
+    <div id="footer">&copy;2016 Completely Fictitious Tennis Inc.
+    </div>
 </div>
-<img src="images/scroll.png" alt="scroll" id="scroll">
-<img src="images/chandelier.gif" alt="light" id="light">
-<img src="images/sofa.gif" alt="sofa" id="sofa">
-<img src="images/rugP.gif" alt="rug" id="rug">
-<img src="images/table.gif" alt="table" id="table">
-<img src="images/sign.jpg" alt="sign" id="sign">
-<img src="images/doorl.gif" alt="doorl" id="doorl">
-<img src="images/doorr.gif" alt="doorl" id="doorr">
-<div id="formdiv">
-<form action="r.php" method="post">
-    Number of Words (&lt;10): <input type="text" name="numwords" maxlength=1 size="1"><br>
 
-   <input type="hidden" name="yesnumb" value="0">
 
-    Include number? <input type="checkbox" name="yesnumb" value="1"> <br>
 
-    <input type="hidden" name="yessymb" value="0">
 
-    Include special character? <input type="checkbox" name="yessymb" value="1"> <br>
 
-    <input type="hidden" name="yescap" value="0">
 
-    Make all caps? <input type="checkbox" name="yescap" value="1"> <br>
 
-    <input type="hidden" name="yesspace" value="0">
+ </body></html> 
+ 
 
-  Separate with spaces not dashes? <input type="checkbox" name="yesspace" value="1"> <br>
-Create your own sepatator instead: <input type="text" name="seps" maxlength=1 size="1"><br>
-<br>
-
-    <input type="submit" value = "Get Password">
-</form>
-<img src="images/arr.png" alt="arrow" id="arrow">
-</div>
-</body>
-</html> 
---
+++ nav.php
@@ -0,0 +1,7 @@
+ 		<ul>
+ 			<li><a class="actlink" href="index.html">Home</a></li>
+ 			<li><a href="backhand.html">Backhand</a></li>
+ 			<li><a href="forehand.html">Forehand</a></li>
+ 			<li><a href="serve.html">Serve</a></li>
+            <li><a href="credits.html">Credits</a></li>
+ 		</ul>
