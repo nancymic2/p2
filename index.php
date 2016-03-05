c3e77113da76f1827b6ed513c56bdba30a3a8a54
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-05 12:24:50 -0500

    p2

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Tennis with Federer</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href='http://fonts.googleapis.com/css?family=Cabin+Condensed:500' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="http://necolas.github.io/normalize.css/">
-	<link rel="stylesheet" href="css/styles.css">
-	<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="js/images2.js"></script>
+		 <?php include("htmlhead.php"); ?> 
+
 </head>
 <body>
  <div class="container"> 
