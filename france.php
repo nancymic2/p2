5267a4dddb77fb0bc667c4f31aa32fb09d9bc44e
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-06 17:08:36 -0500

    p2

--- france.php
+++ france.php
@@ -35,18 +35,25 @@
 	 	    </div>
  		</div>
  				<div class="hundred">
-			<img src="images/saoneLyon.jpg">
+			<img src="images/saoneLyon.jpg" 
+				data-small="images/backhandCred.jpg" 
+	 	    	data-large="images/backhandCred.jpg" 
+	 	    	data-med="images/backhandMed.jpg">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><img alt="france"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
+	 	    <div  class="third"> <p>This may be one of the most famous views of Lyon.
+	 	     I took it from the iconic red Passerelle Saint-Vincent bridge. As the sun began to set each day, the brightly colored buildings on the 
+	 	     River Saone would be awash in an amber glow!</p>
 	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div class="third bord"><p>Lyon is considered to be the food capital of France. And considering, at least in my opinion
+	 	     that France is the food capital of the word, well, it follows that Lyon is the food capital of earth. I can confirm.</p>
 
 	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	  <div class="third"><p>Lyon is the birthplace of 20th-Century haute cuisine. It all began here under the tutelage of Georges Auguste Escoffier. You can
+	 	  	learn more from Anthony Bourdain as he visits Lyon with Daniel Boulud.<a href="https://www.youtube.com/watch?v=pXK2AkDODBM" target="_blank">Parts Unknown</a></p>
 	 	    </div>
  		</div>
-		<div class="hundred">
+		<!--<div class="hundred">
 			<img src="images/lyonCloisters.jpg">
  	    </div>
  	     <div class="middle">
@@ -57,7 +64,7 @@
 	 	    </div>
 	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
 	 	    </div>
- 		</div>
+ 		</div>-->
 		<div class="hundred">
 			<img src="images/fount.jpg" 
 				data-small="images/backhandCred.jpg" 
@@ -72,8 +79,7 @@
 	 	    	adorns the center of the places.</p>
 
 	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
+	 	  <div class="third"><p>Places Des Terreaux is one kilometer in a straight line from Place Bellecour.</p></div>
  		</div>
 
 <div class="hundred">
@@ -93,7 +99,8 @@
 	 	    Apparently in the 1800s Lyon was having an identity crisis and wanted to be more like Paris.</p>
 
 	 	    </div>
-	 	  <div class="third"><p>The huge public spaces in Europe, of which this is one, always amaze me.</p>
+	 	  <div class="third"><p>The huge public spaces in Europe, of which this is one, always amaze me. It also looks a 
+	 	  	it like the grounds at Versailes.</p>
 	 	    </div>
  		</div>
 
