5b425a9bc83486008282101379343b9ad405ae27
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-06 16:39:24 -0500

    p2

--- france.php
+++ france.php
@@ -72,15 +72,23 @@
  		</div>
 
 <div class="hundred">
-			<img src="images/terreaux.jpg">
+			<img src="images/terreaux.jpg" 	 	    	
+				data-small="images/backhandCred.jpg" 
+	 	    	data-large="images/backhandCred.jpg" 
+	 	    	data-med="images/backhandMed.jpg">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><img alt="france"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div  class="third"><p>This is Place Bellecour in Lyon. Up at the top of the hill you can see The Basilica of Notre
+	 	    	-Dame de Fourvière. It was built around 1872.</p>
+
+	 	  
 
 	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	    <div class="third bord"><p>To the right of Fourviere is NOT the Eiffel Tower! But it was intended to look like it. 
+	 	    Apparently in the 1800s Lyon was having an identity crisis and wanted to be more like Paris.</p>
+
+	 	    </div>
+	 	  <div class="third"><p>The huge public spaces in Europe, of which this is one, always amaze me.</p>
 	 	    </div>
  		</div>
 
