9d4af82f1fcd935f3269834c68c29f2c79b90a66
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-05 22:18:07 -0500

    p2

--- index.php
+++ index.php
@@ -10,9 +10,22 @@
  	<div id="middle">
 
  	    <div class="hundred">
-<img src="images/boat.jpg">
+		<img src="images/boat.jpg">
  	    </div>
  	    <div class="middle">
+	 	    <div  class="third"><img alt="tennis photo2"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
+	 	    </div>
+	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+
+	 	    </div>
+	 	    <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	    </div>
+ 		</div>
+
+ 		<div class="hundred">
+		<img src="images/soane.jpg">
+ 	    </div>
+ 	     	    <div class="middle">
 	 	    <div  class="third"><img alt="tennis photo2"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
 	 	    </div>
 	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
