c469f4d1776dbb973a5674a711f3b3dabfd15ef2
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-05 22:39:22 -0500

    p2

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
  		<ul>
  			<li><a class="actlink" href="index.php">Home</a></li>
  			<li><a href="switzerland.php">Switzerland</a></li>
- 			<li><a href="forehand.php">Forehand</a></li>
+ 			<li><a href="england.php">England</a></li>
  			<li><a href="serve.php">Serve</a></li>
             <li><a href="credits.php">Credits</a></li>
  		</ul>
--- smallnav.php
+++ smallnav.php
@@ -1,7 +1,7 @@
  		<ul>
  			<li><a href="index.php">Home</a></li>
  			<li><a href="switzerland.php">Switzerland</a></li>
- 			<li><a class="actlink" href="forehand.php">Forehand</a></li>
+ 			<li><a href="england.php">England</a></li>
  			<li><a href="serve.php">Serve</a></li>
             <li><a href="credits.php">Credits</a></li>
  		</ul>
