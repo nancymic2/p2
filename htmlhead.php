91e06e95e87d4fb049e9599942e1e13e6082f0c6
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-05 23:51:08 -0500

    p2

--- htmlhead.php
+++ htmlhead.php
@@ -6,5 +6,6 @@
 	<link rel="stylesheet" href="css/styles.css">
 	<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="js/images2.js"></script>
