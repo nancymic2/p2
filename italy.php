a75eda703664bb13a5c564ff2050a7bf554bce9e
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-06 11:44:09 -0500

    p2

--- italy.php
+++ italy.php
@@ -7,6 +7,19 @@
 <body>
  <div class="container"> 
 	 <?php include("head.php"); ?> 
+
+		<div class="hundred">
+			<img src="images/colloseo.jpg">
+ 	    </div>
+ 	     <div class="middle">
+	 	    <div  class="third"><img alt="tennis photo2"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
+	 	    </div>
+	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+
+	 	    </div>
+	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	    </div>
+ 		</div>
  	<div id="middle">
 
  	    <div class="hundred">
@@ -34,6 +47,18 @@
 	 	    <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
 	 	    </div>
  		</div>
+ 		 <div class="hundred">
+			<img src="images/fromvenezia.jpg">
+ 	    </div>
+ 	     <div class="middle">
+	 	    <div  class="third"><img alt="tennis photo2"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
+	 	    </div>
+	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+
+	 	    </div>
+	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	    </div>
+ 		</div>
  				<div class="hundred">
 			<img src="images/piazzaduomo.jpg">
  	    </div>
@@ -59,7 +84,7 @@
 	 	    </div>
  		</div>
 		<div class="hundred">
-			<img src="images/fount.jpg">
+			<img src="images/collo2.jpg">
  	    </div>
  	     <div class="middle">
 	 	    <div  class="third"><img alt="tennis photo2"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
@@ -71,8 +96,8 @@
 	 	    </div>
  		</div>
 
-<div class="hundred">
-			<img src="images/terreaux.jpg">
+		<div class="hundred">
+			<img src="images/smalltemple.jpg">
  	    </div>
  	     <div class="middle">
 	 	    <div  class="third"><img alt="tennis photo2"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
