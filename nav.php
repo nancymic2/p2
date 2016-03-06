0eb4a161c99d5255e628b919510e273c4c9f200e
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-05 22:29:56 -0500

    p2

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
  		<ul>
  			<li><a class="actlink" href="index.php">Home</a></li>
- 			<li><a href="backhand.php">Backhand</a></li>
+ 			<li><a href="switzerland.php">Switzerland</a></li>
  			<li><a href="forehand.php">Forehand</a></li>
  			<li><a href="serve.php">Serve</a></li>
             <li><a href="credits.php">Credits</a></li>
