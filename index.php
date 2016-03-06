80ee349597e0991116e463c08e7371901fd23688
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-06 16:18:25 -0500

    p2

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 	 <?php include("head.php"); ?> 
  	<div id="middle">
 
-   		<div id="home" class="hundred">As it turns out, I like pictures. Perhaps I'm tired of reading and writing as I have been reading
+   		<div id="home" class="hundred">As it turns out, I like pictures. Perhaps I'm tired of reading and writing as I
+   		 have been reading
    			for work more than 40 hours a week for 20 years.<br>So take a look at some of my travel photos, and give your reading apparatus
    			a break as well.
    		</div>
--- scotland.php
+++ scotland.php
@@ -20,35 +20,44 @@
 	 	    	townhouses and probably has as many or more than Bath in England.</p>
 	 	    </div>
 	 	    <div class="third bord"><p>
-	 	    	While trying to remember the street this was on, I used Google image search (reverse searh) and nobody ha posted
+	 	    	While trying to remember the street this was on, I used Google image search (reverse search) and nobody has posted
 	 	    	a photo of this, which is shocking.</p>
 
 	 	    </div>
-	 	    <div class="third"><p>Apparently everyone photogrpahs the castle, and perhaps pictures of Fringe, be Edinburgh is a 
+	 	    <div class="third"><p>Apparently everyone photographs the castle, and perhaps pictures of Fringe, be Edinburgh is a 
 	 	    	treasure trove of Georgian houses!
 	 	     </p>
 	 	    </div>
  		</div>
 
  		<div class="hundred">
-		<img src="images/northSea.jpg">
+		<img src="images/northSea.jpg" 			
+			data-small="images/backhandCred.jpg" 
+			data-large="images/backhandCred.jpg" 
+			data-med="images/backhandMed.jpg">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><img alt="scotland"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div  class="third"><p>This is one of my favorite photos from Scotland. This is Leith, where Her Majesty's 
+	 	    	retired yacht is open
+	 	    	to the public. I suppose steering becomes an issue after a certain age.</p></div>
+	 	    <div class="third bord"><p>Leith is on the Firth of Forth, which empties into the North Sea. The firth was as smooth
+	 	    	as glass that day. So serene!</p>
 
 	 	    </div>
-	 	    <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	    <div class="third"><p>Leith is just outside the edge of Edinburgh. There are busses that run there from the city center.
+	 	    	There is a medium-sized retail mall there with some decent restaurants and the tours of the ship.</p>
 	 	    </div>
  		</div>
  				<div class="hundred">
-			<img src="images/britannia.jpg">
+			<img src="images/britannia.jpg"			
+			data-small="images/backhandCred.jpg" 
+			data-large="images/backhandCred.jpg" 
+			data-med="images/backhandMed.jpg">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><img alt="scotland"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div  class="third"> <p>In the background are some new housing towers. As I looked over the magically named Firth of Forth, 
+	 	    	I thought they looked like a fantastic place to live.</div>
+	 	    <div class="third bord"><p></p>
 
 	 	    </div>
 	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
