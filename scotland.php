6cc71c6dd30e44f2433b6c0d01fab8cbee050941
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-06 16:08:54 -0500

    p2

--- scotland.php
+++ scotland.php
@@ -17,12 +17,16 @@
  	    </div>
  	    <div class="middle">
 	 	    <div  class="third"><p>One of the many crescent-shaped terrace developments in Edinburgh. Edinburgh is known for its 18th Century
-	 	    	townhouses and probably has as many ot more than Bath in England.</p>
+	 	    	townhouses and probably has as many or more than Bath in England.</p>
 	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div class="third bord"><p>
+	 	    	While trying to remember the street this was on, I used Google image search (reverse searh) and nobody ha posted
+	 	    	a photo of this, which is shocking.</p>
 
 	 	    </div>
-	 	    <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	    <div class="third"><p>Apparently everyone photogrpahs the castle, and perhaps pictures of Fringe, be Edinburgh is a 
+	 	    	treasure trove of Georgian houses!
+	 	     </p>
 	 	    </div>
  		</div>
 
