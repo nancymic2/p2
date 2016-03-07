2a82b11775b9e3a9d605feb674c64779afa4f98f
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-07 18:10:21 -0500

    p2

--- switzerland.php
+++ switzerland.php
@@ -46,21 +46,22 @@ To make records with a mobile
 We didn't have much time
 Frank Zappa and the Mothers
 Were at the best place around
-But some stupid with a flare gun
-Burned the place to the ground
+
 
 </p>
 	 	    </div>
-	 	    <div class="third bord"><p>They burned down the gambling house
+	 	    <div class="third bord"><p>But some stupid with a flare gun
+Burned the place to the ground. They burned down the gambling house
 It died with an awful sound
 Funky Claude was running in and out
 Pulling kids out the ground
-When it all was over
+</p>
+	 	    </div>
+	 	    <div class="third"><p>
+	 	    	<p>When it all was over
 We had to find another place
 But Swiss time was running out
 It seemed that we would lose the race.</p>
-	 	    </div>
-	 	    <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
 	 	    </div>
  		</div>
  		<div class="hundred">
@@ -83,7 +84,8 @@ I know, I know we'll never forget
 
 	 	    </div>
 	 	  	<div class="third"><p>Smoke on the water, a fire in the sky
-Smoke on the water</p> </div>
+Smoke on the water...</p> As you might guess, this very old song by Deep Purple is about Montreux on Laake Geneva. Montreux is also
+home to a number of music festivals.</div>
  		</div>
 <div class="hundred">
 			<img src="images/un.jpg">
