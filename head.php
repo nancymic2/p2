4955614013914ad575e455e14f4c3b19ee82b935
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-07 08:58:11 -0500

    p2

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
- 	<header id="header1"><span id="headline">Travels with Charlie <span id="slug">the Camera</span><img src="images/hamb.png" id="hamburger" alt="hamburger"></span>
+ 	<header id="header1"><span id="headline">Travels with Charlie <span id="slug">the Camera
+ 	</span><a href="#" onclick="getmenu();"><img src="images/hamb.png" id="hamburger" alt="hamburger"></a></span>
  		
  	</header>
  	<div id="header"></div>
