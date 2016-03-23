46e65973a9c6f4ce2901a4fe0afcee4db0a01859
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-23 16:46:25 -0400

    add page more

--- more.php
+++ more.php
@@ -14,20 +14,20 @@
  
 <div class="leftdiv">
 <h4>Right Signs</h4>
-Purus praesent consequat nibh purus non mi dictum curabitur, diam egestas eleifend fusce dapibus aptent placerat donec curabitur, blandit faucibus mollis netus duis erat ultrices. Curae sem habitasse erat facilisis aptent vel lobortis scelerisque, turpis nam ad nisl tempor quam pretium fermentum, fringilla mollis amet primis fusce est dui. Tellus accumsan elit tempor 
+<p id="par1">Purus praesent consequat nibh purus non mi dictum curabitur, diam egestas eleifend fusce dapibus aptent placerat donec curabitur, blandit faucibus mollis netus duis erat ultrices. Curae sem habitasse erat facilisis aptent vel lobortis scelerisque, turpis nam ad nisl tempor quam pretium fermentum, fringilla mollis amet primis fusce est dui. Tellus accumsan elit tempor 
 <img src="images/pointright.jpg" class="left">
 
-interdum quisque torquent mattis elementum imperdiet dui, viverra molestie per etiam curabitur nunc sodales orci nisl, proin porta vehicula integer felis bibendum congue maecenas feugiat. Consequat venenatis varius scelerisque leo scelerisque, platea ultrices venenatis platea aliquam imperdiet, ligula aliquam nisi urna. Pellentesque in nisl nullam sagittis lectus augue morbi himenaeos, eros lobortis felis vestibulum nulla conubia praesent non gravida, adipiscing vestibulum elit vulputate class accumsan felis. Himenaeos nullam fermentum vulputate ornare dapibus potenti quisque blandit, dictumst aenean dictumst conubia cras nullam inceptos tellus, vestibulum metus viverra adipiscing aliquet euismod aliquet.
+interdum quisque torquent mattis elementum imperdiet dui, viverra molestie per etiam curabitur nunc sodales orci nisl, proin porta vehicula integer felis bibendum congue maecenas feugiat. Consequat venenatis varius scelerisque leo scelerisque, platea ultrices venenatis platea aliquam imperdiet, ligula aliquam nisi urna. Pellentesque in nisl nullam sagittis lectus augue morbi himenaeos, eros lobortis felis vestibulum nulla conubia praesent non gravida, adipiscing vestibulum elit vulputate class accumsan felis. Himenaeos nullam fermentum vulputate ornare dapibus potenti quisque blandit, dictumst aenean dictumst conubia cras nullam inceptos tellus, vestibulum metus viverra adipiscing aliquet euismod aliquet.</p>
 </div>
 
 
 <div class="rightdiv">
 <h3>Left Signs</h3>
-Feugiat nullam duis phasellus urna pharetra ad, dictumst eleifend dui nam sollicitudin, cubilia dolor vulputate mattis vivamus. Volutpat elementum nostra consectetur torquent lacus vestibulum, nisl mi quisque semper tellus praesent posuere, quis pellentesque et dapibus posuere. Platea quis torquent ullamcorper commodo sem nec diam volutpat ipsum, maecenas at eros maecenas lorem etiam morbi eu, lobortis tristique arcu laoreet conubia tellus amet velit. Habitant vivamus vestibulum hac egestas potenti
+<p id="par2">Feugiat nullam duis phasellus urna pharetra ad, dictumst eleifend dui nam sollicitudin, cubilia dolor vulputate mattis vivamus. Volutpat elementum nostra consectetur torquent lacus vestibulum, nisl mi quisque semper tellus praesent posuere, quis pellentesque et dapibus posuere. Platea quis torquent ullamcorper commodo sem nec diam volutpat ipsum, maecenas at eros maecenas lorem etiam morbi eu, lobortis tristique arcu laoreet conubia tellus amet velit. Habitant vivamus vestibulum hac egestas potenti
 
 <img src="images/pointleft.jpg" class="right">
 
- vitae lectus habitasse sodales inceptos, massa venenatis pharetra mollis morbi suspendisse at vel erat, ut nisl pulvinar interdum ultricies interdum per auctor egestas. Consequat dictum cursus consectetur ut lectus scelerisque felis sem, consequat in netus scelerisque ut nullam amet accumsan, augue consectetur nunc aenean porta aptent sodales. Auctor magna eget torquent eget mauris hendrerit vestibulum nulla mattis, lectus consequat non curae sapien est sodales. Elit consequat mattis convallis aliquet phasellus. Tharetra ad, dictumst eleifend dui nam sollicitudin, cubilia dolor vulputate mattis vivamus.
+ vitae lectus habitasse sodales inceptos, massa venenatis pharetra mollis morbi suspendisse at vel erat, ut nisl pulvinar interdum ultricies interdum per auctor egestas. Consequat dictum cursus consectetur ut lectus scelerisque felis sem, consequat in netus scelerisque ut nullam amet accumsan, augue consectetur nunc aenean porta aptent sodales. Auctor magna eget torquent eget mauris hendrerit vestibulum nulla mattis, lectus consequat non curae sapien est sodales. Elit consequat mattis convallis aliquet phasellus. Tharetra ad, dictumst eleifend dui nam sollicitudin, cubilia dolor vulputate mattis vivamus.</p>
 </div>
 
     </div>
