f21fe832e35860ef0786e5bf47063add550656a6
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-23 12:08:32 -0400

    add page more

--- more.php
+++ more.php
@@ -25,7 +25,7 @@ Feugiat nullam duis phasellus urna pharetra ad, dictumst eleifend dui nam sollic
 
 <img src="images/pointleft.jpg" class="right">
 
- vitae lectus habitasse sodales inceptos, massa venenatis pharetra mollis morbi suspendisse at vel erat, ut nisl pulvinar interdum ultricies interdum per auctor egestas. Consequat dictum cursus consectetur ut lectus scelerisque felis sem, consequat in netus scelerisque ut nullam amet accumsan, augue consectetur nunc aenean porta aptent sodales. Auctor magna eget torquent eget mauris hendrerit vestibulum nulla mattis, lectus consequat non curae sapien est sodales. Elit consequat mattis convallis aliquet phasellus.
+ vitae lectus habitasse sodales inceptos, massa venenatis pharetra mollis morbi suspendisse at vel erat, ut nisl pulvinar interdum ultricies interdum per auctor egestas. Consequat dictum cursus consectetur ut lectus scelerisque felis sem, consequat in netus scelerisque ut nullam amet accumsan, augue consectetur nunc aenean porta aptent sodales. Auctor magna eget torquent eget mauris hendrerit vestibulum nulla mattis, lectus consequat non curae sapien est sodales. Elit consequat mattis convallis aliquet phasellus. Tharetra ad, dictumst eleifend dui nam sollicitudin, cubilia dolor vulputate mattis vivamus.
 </div>
 
     </div>
