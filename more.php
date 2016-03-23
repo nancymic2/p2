df55146f5cb8a78ed9e41e27a0512d5802b81c51
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-23 10:35:35 -0400

    add page more

--- more.php
+++ more.php
@@ -6,112 +6,18 @@
 </head>
 <body>
  <div class="container"> 
-	 <?php include("head.php"); ?> 
-	<div id="middle">
-		<div class="hundred title" id="titlebar">Italy</div>
-		<div class="hundred">
-			<img src="images/colloseo.jpg">
- 	    </div>
- 	     <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
-
-	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>
+	  <?php include("head.php"); ?> 
+	  <div id="middle">
+		<div class="hundred title" id="titlebar">More Stuff</div>
+		<div class="hundred"> </div>
+ 	 
  
 
- 	    <div class="hundred">
-		<img src="images/portovenere.jpg">
- 	    </div>
- 	    <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
 
-	 	    </div>
-	 	    <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>
 
- 		<div class="hundred">
-		<img src="images/vernazza.jpg">
- 	    </div>
- 	     <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
-
-	 	    </div>
-	 	    <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>
- 		 <div class="hundred">
-			<img src="images/fromvenezia.jpg">
- 	    </div>
- 	     <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
-
-	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>
- 				<div class="hundred">
-			<img src="images/piazzaduomo.jpg">
- 	    </div>
- 	     <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
-
-	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>
-		<div class="hundred">
-			<img src="images/varenna.jpg">
- 	    </div>
- 	     <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
-
-	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>
-		<div class="hundred">
-			<img src="images/collo2.jpg">
- 	    </div>
- 	     <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
-
-	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>
-
-		<div class="hundred">
-			<img src="images/smalltemple.jpg">
- 	    </div>
- 	     <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
-
-	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>
 
     </div>
-<?php include("footer.php"); ?> 
+    <?php include("footer.php"); ?> 
 </div>
 
 </body></html> 
