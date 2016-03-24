32f041b9314842b14df4c93480419b11c7076c0e
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-24 14:39:52 -0400

    switch to sass

--- england.php
+++ england.php
@@ -30,12 +30,12 @@
 		<img src="images/sheldonian.jpg" alt="england">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><img alt="england"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
+	 	    <div  class="third"><p>This the Sheldonian Theatre. Graduations are held here. This building was designed by Sir Christopher Wren. Like the Radcliffe Camera, it is an iconic sight in Oxford.</p>
 	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div class="third bord"><p>Check out the men with the bowler hats just peeking out above the wall. Apparently these are not all the original heads. At least I think I've heard that is the case.</p>
 
 	 	    </div>
-	 	    <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	    <div class="third"><p>To the right of the Sheldonian Theatre is the Museum of the History of Science. Very cool museum. It was the original site of the Ashmolean Museum in Oxford. It also was designed by Christopher Wren.</p>
 	 	    </div>
  		</div>
 
@@ -43,12 +43,12 @@
 			<img src="images/balliol.jpg" alt="england">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><img alt="england"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
+	 	    <div  class="third"><p>This is inside the dining hall of Balliol College. I took videos as well walking around here. Very historic.</p>
 	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div class="third bord"><p>There are 38 colleges of Oxford University. Balliol is one of the more prminent ones along with Christ Church and Brasenose.</p>
 
 	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	  <div class="third"><p>The college system is different there. I can't remember exactly but in some circumstances you decide which hall to join.</p>
 	 	    </div>
  		</div>
 
@@ -56,24 +56,24 @@
 			<img src="images/eye.jpg" alt="england">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><img alt="england"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
+	 	    <div  class="third"><p>This is the London Eye. It was built originally for the millennial celebration. I've been there a few times but still have not had the time to go up, but the views over the Thames must be great.</p>
 	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div class="third bord"><p>Next to or near the Eye is the millennium bridge, which is a pedestrian bridge also built for the millennial celebration.</p>
 
 	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	  <div class="third"><p>One end of the bridge is near the Globe theatre, which still has a thatched roof, even though it is a replica of the original and was built in the 1990's.</p>
 	 	    </div>
  		</div>
  		<div class="hundred">
 			<img src="images/oxfordst.jpg" alt="england">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><img alt="england"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
+	 	    <div  class="third"><p>This is Oxford Street in the middle of London. During the holiday season, it is one of the first streets decorated. This was a warm night in November.</p>
 	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
+	 	    <div class="third bord"><p>On Oxford Street is Selfridge's, made famous here n the PBS series called Mr. Selfridge. Last year when we were there there was a gold Paddington Bear in the window.</p>
 
 	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
+	 	  <div class="third"><p>Oxford Street has lots of shopping as well, including lots of well-known stores, but not Harrods.</p>
 	 	    </div>
  		</div>
 
