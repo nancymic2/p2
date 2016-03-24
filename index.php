5a6b42cc780164de6c4653b3ce8ab8dddc9d4137
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-24 10:55:48 -0400

    switch to sass

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
    		<div id="home" class="hundred">As it turns out, I like pictures. Perhaps I'm tired of reading and writing as I
    		 have been reading
    			for work more than 40 hours a week for 20 years.<br>So take a look at some of my travel photos, and give your eyes a break as well.<br>
-        As a big sailing fan, water and boats figure prominently in photos I take, so here's a favorite:
+        As a big sailing fan, water and boats figure prominently in photos I take, so here's a favorite from the port in Nice last week:
    		</div>
 
  		<div class="hundred">
