9d6ae38727b62222f3b78f70b6ef6c09d9148d28
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-24 08:51:47 -0400

    switch to sass

--- htmlhead.php
+++ htmlhead.php
@@ -3,7 +3,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='http://fonts.googleapis.com/css?family=Cabin+Condensed:500' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="http://necolas.github.io/normalize.css/">
-	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="stylesheets/styles.css">
 	<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'>
