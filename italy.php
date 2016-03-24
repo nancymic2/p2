f5260077ca906e756817ae1df717976a6d3d7956
Nancy Michell <nancy.ann.michell@hp.com>
2016-03-24 19:12:50 -0400

    home

--- italy.php
+++ italy.php
@@ -84,24 +84,12 @@
 	 	  <div class="third"><p>The biggest popular town is probably Bellagio. It is at the other end of the lake from Como where the train ends so you can either take a fabulous bus ride over the cliffs, a slow ferry, or a fast ferry. It's an hour basically. It's a long lake.</p>
 	 	    </div>
  		</div>
-		<!--<div class="hundred">
-			<img src="images/collo2.jpg" alt="italy">
- 	    </div>
- 	     <div class="middle">
-	 	    <div  class="third"><img alt="italy"  class="space serveImg"  src="images/backhandCred.jpg" data-small="images/backhandCred.jpg" data-large="images/backhandCred.jpg" data-med="images/backhandMed.jpg">
-	 	    </div>
-	 	    <div class="third bord"><p>Roger Federer September 18, 2013. Preparing his backhand. Note the grip and stance. This is the right way to set up for a one-hand backhand. Note hpw Federer cradles the throat of the racquet in his left hand.</p>
-
-	 	    </div>
-	 	  <div class="third"><p>From wikimedia free to use. <a href="https://commons.wikimedia.org/wiki/File:Federer_RG13_(62)_(9410842993).jpg" target="_blank">Photo</a></p>
-	 	    </div>
- 		</div>-->
 
 		<div class="hundred">
 			<img src="images/smalltemple.jpg" alt="italy">
  	    </div>
  	     <div class="middle">
-	 	    <div  class="third"><p>This little temple in Rome, the Temple of Portunus sits next to a similar round one, the Temple of Vesta. What's interesting is that they are about 200 years older than the colosseum, yet they don't seem to get much attention.</p>
+	 	    <div  class="third"><p>This little temple in Rome, the Temple of Portunus sits next to a similar round one, the <a href="http://www.ancient.eu/article/625/" target="_blank">Temple of Vesta</a>. What's interesting is that they are about 200 years older than the colosseum, yet they don't seem to get much attention.</p>
 	 	    </div>
 	 	    <div class="third bord"><p>The setting is close to the Tiber, a little past Piazza Venezia. I don't know why people aren't flocking there.</p>
 
