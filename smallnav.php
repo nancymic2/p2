91de2ad174c7189a890e21f7023a224cc6ce1137
Nancy Michell <nancy.ann.michell@hp.com>
2016-10-13 16:30:29 -0400

    nav

--- smallnav.php
+++ smallnav.php
@@ -5,5 +5,5 @@
  			<li><a href="france.php">France</a></li>
  			<li><a href="scotland.php">Scotland</a></li>
             <li><a href="italy.php">Italy</a></li>
-            <li><a href="more.php">More</a></li>
+            <!--<li><a href="more.php">More</a></li>-->
    		</ul>
